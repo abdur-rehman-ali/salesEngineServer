de71e3dc8cf7a5c9a9d8fd489b934e557e52ef8e
abdur-rehman-ali <alijoyia911@gmail.com>
2024-08-08 18:07:15 -0700

    formatted created_at and updated_at in serializers file

--- technologies/serializers.py
+++ technologies/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Technology
 
 class TechnologySerializer(serializers.ModelSerializer):
+  created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+  updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+
   class Meta:
     model = Technology
     fields = ['id', 'name', 'created_at', 'updated_at']
