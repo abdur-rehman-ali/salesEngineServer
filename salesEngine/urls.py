9f8915d34520e6cad3ee3dd01253733b4acf1a62
abdur-rehman-ali <alijoyia911@gmail.com>
2024-08-12 11:33:47 -0700

    Code refactored

--- salesEngine/urls.py
+++ salesEngine/urls.py
@@ -17,12 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/v1/', include('technologies.urls')),
-#     path('api/v1/', include('pseudoProfiles.urls')),
-# ]
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/technologies/', include('technologies.urls')),
